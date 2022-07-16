3b72c16fb2c6c0d25ac25c3ea6562bebc08ed2d6
EC2 Default User <ec2-user@ip-172-31-2-20.ec2.internal>
2022-07-16 08:40:24 +0000

    Initial Commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Book.create(title:'リーダブルコード',body:'より良いコードを書くためのシンプルで実践的なテクニック')
-Book.create(title:'トラブル知らずのシステム設計',body:'システム設計の要点を図解で説明してくれます')
-Book.create(title:'たのしいRuby',body:'Rubyの入門におすすめです')
-Book.create(title:'入門Git',body:'gitについての基本操作から内部構造まで解説してくれます')
-Book.create(title:'アルゴリズム図鑑',body:'アルゴリズムについてカラーイラストでしっかり解説してくれます')
-Book.create(title:'データベース実践入門',body:'効率的なSQL文を教えてくれます')
-Book.create(title:'達人プログラマー',body:'プログラマ入門者は必読')
+Book.create(id:85763,title:'リーダブルコード',body:'より良いコードを書くためのシンプルで実践的なテクニック')
+Book.create(id:85764,title:'トラブル知らずのシステム設計',body:'システム設計の要点を図解で説明してくれます')
+Book.create(id:85765,title:'たのしいRuby',body:'Rubyの入門におすすめです')
+Book.create(id:85766,title:'入門Git',body:'gitについての基本操作から内部構造まで解説してくれます')
+Book.create(id:85767,title:'アルゴリズム図鑑',body:'アルゴリズムについてカラーイラストでしっかり解説してくれます')
+Book.create(id:85768,title:'データベース実践入門',body:'効率的なSQL文を教えてくれます')
+Book.create(id:85769,title:'達人プログラマー',body:'プログラマ入門者は必読')
 
